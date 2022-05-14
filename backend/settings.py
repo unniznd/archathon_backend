29f23a79a300024385a19dbbab5cab8e5deb9608
unniznd <au6695@gmail.com>
2022-05-14 17:15:32 +0530

    dnspython added

--- backend/settings.py
+++ backend/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# if DEBUG:
-#     from .dev_settings import *
-# else:
-from .prod_settings import *
+if DEBUG:
+    from .dev_settings import *
+else:
+    from .prod_settings import *
 
 
 
