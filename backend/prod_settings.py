c4607ced26b2cbcc2e4ea5ea7593ee319c98d356
unniznd <au6695@gmail.com>
2022-05-14 17:52:33 +0530

    database changed to postgresql

--- backend/prod_settings.py
+++ backend/prod_settings.py
@@ -1,17 +1,19 @@
 import os
-
+import json
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = ['archathonbackendserver.herokuapp.com','*']
+ALLOWED_HOSTS = ['archathonbackendserver.herokuapp.com',]
 
+with open('secret_key.json') as f:
+    secret = json.load(f)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'myFirstDatabase',
-        'CLIENT': {
-            'host': os.environ.get("HOST")
-        }  
+        "default": {
+            "ENGINE": secret['ENGINE'],
+            "NAME": secret['NAME'],
+            "USER": secret['USER'],
+            "PASSWORD":os.environ.get('PASSWORD'),
+            "HOST":secret['HOST'],
+            "PORT": secret['PORT']
+        }
     }
-}
-
--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     from .dev_settings import *
